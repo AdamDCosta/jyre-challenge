8ecaf1d9d6981035c3ae4127a17a802fd3a7f769
AdamDCosta <adam.dcosta@googlemail.com>
2021-12-03 10:41:28 +0000

    leader page html boilerplate added

---
+++ src/components/LeaderPage/LeaderPage.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styles from "./LeaderPage.module.scss";
+
+const LeaderPage = (props) => {
+  const { leaders } = props;
+
+  const leaderColour = "props....color";
+
+  return (
+    <section>
+      <div>
+        <div>
+          <h1>Title</h1>
+          <h2>Quote</h2>
+          <h3> - "Leader"</h3>
+        </div>
+        <div>
+          <img src="#" alt="leader" />
+        </div>
+      </div>
+      <article>
+        <h2>The Character of the Innovator</h2>
+        <p>Description</p>
+      </article>
+      <aside>
+        <ul>
+          <li>Leader Person</li>
+        </ul>
+      </aside>
+      <div>
+        <div>
+          <h3>At their best</h3>
+          <ul>
+            <li>etc</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Dark side</h3>
+          <ul>
+            <li>
+              etc...
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LeaderPage;
