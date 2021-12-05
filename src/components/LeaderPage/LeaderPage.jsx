65362da76aea36bf3033857c1132ba9f86fbf0f4
AdamDCosta <adam.dcosta@googlemail.com>
2021-12-05 10:44:48 +0000

    close leader window link added with cross icon

--- src/components/LeaderPage/LeaderPage.jsx
+++ src/components/LeaderPage/LeaderPage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { link } from 'react-router-dom'
 import styles from "./LeaderPage.module.scss";
+import Card from "../Card/Card";
+import cross from "../../assets/images/white-cross.png";
 
 const LeaderPage = (props) => {
   const { leaders } = props;
@@ -78,54 +81,60 @@ const LeaderPage = (props) => {
   console.log(leaderBestTitle);
 
   return (
-    <section className={styles["leader-page"]}>
-      <div className={styles["leader-page__heading"]}>
-        <div className={styles["leader-page__heading--text"]}>
-          <h1
-            className={styles["leader-page__heading--text-title"]}
-            style={{ color: leaderColour }}
-          >
-            {leaderObject.name[0].text}
-          </h1>
-          <h2 className={styles["leader-page__heading--text-quote"]}>
-            {leaderObject.quote}
-          </h2>
-          <h3 className={styles["leader-page__heading--text-exemplar"]}>
-            {leaderObject.exemplar}
+    <Card>
+      <section className={styles["leader-page"]}>
+            <Link to="/">
+              <img className={styles["leader-page__cross"]} src={cross} alt="close window" />
+            </Link>
+        <div className={styles["leader-page__heading"]}>
+          <div className={styles["leader-page__heading--text"]}>
+            <h1
+              className={styles["leader-page__heading--text-title"]}
+              style={{ color: leaderColour }}
+            >
+              {leaderObject.name[0].text}
+            </h1>
+            <h2 className={styles["leader-page__heading--text-quote"]}>
+              {leaderObject.quote}
+            </h2>
+            <h3 className={styles["leader-page__heading--text-exemplar"]}>
+              {leaderObject.exemplar}
+            </h3>
+          </div>
+          <figure className={styles["leader-page__heading--image"]}>
+            <img
+              src={leaderObject.exemplar_image.url}
+              alt={leaderObject.exemplar_image.alt}
+            />
+          </figure>
+        </div>
+        <article className={styles["leader-page__character"]}>
+          <h3 className={styles["leader-page__character--title"]}>
+            {descriptionTitle}
           </h3>
-        </div>
-        <figure className={styles["leader-page__heading--image"]}>
-          <img
-            src={leaderObject.exemplar_image.url}
-            alt={leaderObject.exemplar_image.alt}
-          />
-        </figure>
-      </div>
-      <article className={styles["leader-page__character"]}>
-        <h3 className={styles["leader-page__character--title"]}>
-          {descriptionTitle}
-        </h3>
-        <p className={styles["leader-page__character--desc"]}>
-          {descriptionParagraph}
-        </p>
-      </article>
-      <article className={styles["leader-page__lists"]}>
-        <div
-          className={styles["leader-page__lists--best"]}
-          style={{ border: `1px solid ${leaderColour}` }}
-        >
-          <h4 style={{ color: leaderColour }}>{leaderBestTitle}</h4>
-          <ul>{bestListJSX}</ul>
-        </div>
-        <div
-          className={styles["leader-page__lists--dark"]}
-          style={{ border: `1px solid ${leaderColour}` }}
-        >
-          <h4 style={{ color: leaderColour }}>{leaderDarkTitle}</h4>
-          <ul>{darkListJSX}</ul>
-        </div>
-      </article>
-    </section>
+          <p className={styles["leader-page__character--desc"]}>
+            {descriptionParagraph}
+          </p>
+        </article>
+        <article className={styles["leader-page__lists"]}>
+          <div
+            className={styles["leader-page__lists--best"]}
+            style={{ border: `1px solid ${leaderColour}` }}
+          >
+            <h4 style={{ backgroundColor: leaderColour }}>{leaderBestTitle}</h4>
+            <ul>{bestListJSX}</ul>
+          </div>
+          <div
+            className={styles["leader-page__lists--dark"]}
+            style={{ border: `1px solid ${leaderColour}` }}
+          >
+            <h4 style={{ backgroundColor: leaderColour }}>{leaderDarkTitle}</h4>
+            <ul>{darkListJSX}</ul>
+          </div>
+        </article>
+      </section>
+
+    </Card>
   );
 };
 
